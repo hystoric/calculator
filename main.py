e632950c0711f203738646e8b3714d73082e78be
hysterickx <theendoffatuity7@gmail.com>
2026-05-29 18:47:54 +0300

    Refactor: migrate to OOP and CustomTkinter, update UI design

--- main.py
+++ main.py
@@ -1,132 +1,160 @@
 import customtkinter as ctk
 from CTkMessagebox import CTkMessagebox
+from functools import partial
 
-def clear():
-    calc.delete(0, 'end')
-    calc.insert (0,0)
+class CalculatorApp(ctk.CTk):
+    def __init__(self):
+        super().__init__()
 
-def calculate():
-    value = calc.get()
-    if value [-1] in '+-*/':
-        value += value [:-1]
-    calc.delete(0, 'end')
-    try:
-        calc.insert(0, eval(value))
-    except (NameError, SyntaxError):
-        error_message = CTkMessagebox(
-            win,
-            title='Внимание!',
-            message='Нужны только цифры. Вы ввели другие символы!'
+        ctk.set_appearance_mode("dark")
+        self.title('Калькулятор')
+        self.geometry('300x340+1000+500')
+        self.resizable(False, False)
+
+        self.grid_params = {
+            'sticky': 'wens',
+            'padx': 5, 'pady': 5
+        }
+
+        self.btn_params = {
+            'fg_color': '#333333',
+            'hover_color': '#262626',
+            'font': ('Arial', 13)
+        }
+
+        self.create_ui()
+        self.configure_grid()
+
+    def create_ui(self):
+        self.entry = ctk.CTkEntry(
+            self, justify='right',
+            font=('Arial', 30), height=50
         )
-        calc.insert(0,0)
-    except ZeroDivisionError:
-        error_message = CTkMessagebox(
-            win,
-            title='Внимание!',
-            message='На ноль делить нельзя!'
+        self.entry.insert(0, '0')
+        self.entry.grid(
+            row=0, column=0,
+            columnspan=4,
+            stick='we', padx=5
         )
-        calc.insert(0,0)
+        self.entry.bind('<Key>', self.calculate)
 
-def add_digit(digit):
-    value = calc.get()
-    if value[0] == '0' and len(value) == 1:
-        value = value[1:]
-    calc.delete(0, 'end')
-    calc.insert(0, value + digit)
+        button_data = [
+            ('0', 4, 0), ('1', 1, 0), ('2', 1, 1), ('3', 1, 2),
+            ('4', 2, 0), ('5', 2, 1), ('6', 2, 2), ('7', 3, 0),
+            ('8', 3, 1), ('9', 3, 2), ('+', 1, 3), ('-', 2, 3),
+            ('*', 3, 3), ('/', 4, 3), ('C', 4, 1), ('=', 4, 2)
+        ]
 
-def add_operation(operation):
-    value = calc.get()
-    if value[-1] in '+-*/':
-        value = value[:-1]
-    elif '+' in value or '-' in value or '*' in value or '/' in value:
-        calculate()
-        value = calc.get()
-    calc.delete(0, 'end')
-    calc.insert(0, value + operation)
+        for text, row, column in button_data:
+            command = partial(self.calculate, text)
 
-def press_key(event):
-    print(repr(event.char))
-    if event.char.isdigit():
-        add_digit(event.char)
-    elif event.char in '+-*/':
-        add_operation(event.char)
-    elif event.char == '\r':
-        calculate()
+            button = ctk.CTkButton(
+                self,
+                **self.btn_params,
+                text=text,
+                command=command
+            )
+            button.grid(
+                row=row,
+                column=column,
+                **self.grid_params
+            )
 
-ctk.set_appearance_mode("dark")
-win = ctk.CTk()
-win.title('Калькулятор')
-win.bind ('<Key>', press_key)
-win.geometry('300x340+600+300')
-win.resizable(False, False)
+    def configure_grid(self):
+        for column in range(4):
+            self.grid_columnconfigure(column, weight=1)
 
-calc = ctk.CTkEntry(
-    win, justify='right', font=('Arial', 30),
-    width=15, height = 50
-)
-calc.insert(0, '0')
-calc.grid(
-    row=0, column=0, columnspan=4,
-    stick='we', padx=5
-)
+        for row in range(1, 5):
+            self.grid_rowconfigure(row, weight=1)
 
-GRID_PARAMS = {
-    'sticky': 'wens',
-    'padx': 5,
-    'pady': 5
-}
+    def calculate(self, action):
+        is_keyboard = hasattr(action, 'char')
 
-BTN_PARAMS = {
-    'fg_color': '#333333',
-    'hover_color': '#262626',
-    'font': ('Arial', 13)
-}
+        if is_keyboard:
+            event = action
+            action = event.char
 
-button_data = [
-    ('0', 4, 0, lambda: add_digit('0')),
-    ('1', 1, 0, lambda: add_digit('1')),
-    ('2', 1, 1, lambda: add_digit('2')),
-    ('3', 1, 2, lambda: add_digit('3')),
-    ('4', 2, 0, lambda: add_digit('4')),
-    ('5', 2, 1, lambda: add_digit('5')),
-    ('6', 2, 2, lambda: add_digit('6')),
-    ('7', 3, 0, lambda: add_digit('7')),
-    ('8', 3, 1, lambda: add_digit('8')),
-    ('9', 3, 2, lambda: add_digit('9')),
-    ('+', 1, 3, lambda: add_operation('+')),
-    ('-', 2, 3, lambda: add_operation('-')),
-    ('*', 3, 3, lambda: add_operation('*')),
-    ('/', 4, 3, lambda: add_operation('/')),
-    ('C', 4, 1, clear),
-    ('=', 4, 2, calculate)
-]
+            if event.keysym == 'Backspace' or action == '\x08':
+                value = self.entry.get()
+                self.entry.delete(0, 'end')
 
-for text, row, column, command in button_data:
-    button = ctk.CTkButton(
-        win,
-        **BTN_PARAMS,
-        text=text,
-        command=command
-    )
+                if len(value) <= 1:
+                    self.entry.insert(0, '0')
+                else:
+                    self.entry.insert(0, value[:-1])
+                return 'break'
 
-    button.grid(
-        row=row,
-        column=column,
-        **GRID_PARAMS
-    )
+            if action == '\r':
+                action = '='
 
-grid_types = {
-    'column': win.grid_columnconfigure,
-    'row': win.grid_rowconfigure
-}
+            if action not in '0123456789+-*/=':
+                return 'break'
 
-grid_data = [
-    ('column', 0), ('column', 1), ('column', 2), ('column', 3),
-    ('row', 1), ('row', 2), ('row', 3), ('row', 4)
-]
+        value = self.entry.get()
 
-for grid_type, value in grid_data:
-    configure = grid_types[grid_type]
-    configure(value, weight=1)
+        if action == 'C':
+            self.entry.delete(0, 'end')
+            self.entry.insert(0,0)
+            return 'break' if is_keyboard else None
 
-win.mainloop()
+        if action == '=':
+            if not value:
+                return 'break' if is_keyboard else None
+
+            if value [-1] in '+-*/':
+                value += value [:-1]
+            self.entry.delete(0, 'end')
+
+            try:
+                result = str(eval(value))
+                self.entry.insert(0, result)
+            except (NameError, SyntaxError):
+                CTkMessagebox(
+                    self,
+                    title='Внимание!',
+                    message='Нужны только цифры. Вы ввели другие символы!'
+                )
+                self.entry.insert(0,0)
+            except ZeroDivisionError:
+                CTkMessagebox(
+                    self,
+                    title='Внимание!',
+                    message='На ноль делить нельзя!'
+                )
+                self.entry.insert(0,0)
+            return 'break' if is_keyboard else None
+
+        if action in '+-*/':
+            if not value:
+                return 'break' if is_keyboard else None
+            if value[-1] in '+-*/':
+                value = value[:-1]
+            elif any(op in value for op in '+-*/'):
+                self.entry.delete(0, 'end')
+                try:
+                    value = str(eval(value))
+                except ZeroDivisionError:
+                    CTkMessagebox(
+                        self,
+                        title='Внимание!',
+                        message='На ноль делить нельзя!'
+                    )
+                    value = '0'
+                except Exception:
+                    value = '0'
+            self.entry.delete(0, 'end')
+            self.entry.insert(0, value + action)
+            return 'break' if is_keyboard else None
+
+        if value == '0':
+            value = ''
+        self.entry.delete(0, 'end')
+        self.entry.insert(0, value + str(action))
+
+        if is_keyboard:
+            return 'break'
+
+
+if __name__ == '__main__':
+    app = CalculatorApp()
+    app.mainloop()
